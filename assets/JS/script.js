9193d9cd62bc86d17807280c4dcf77d08289cb04
Felipe Borges <felipedefreitasborges@gmail.com>
2026-09-14 21:21:30 -0300

    Delete assets directory

--- assets/JS/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    document.querySelector(".menuMobile").addEventListener("click", function () {
-        if (document.querySelector("nav ul").style.display == 'flex') {
-            document.querySelector("nav ul").style.display = 'none';
-        } else {
-            document.querySelector("nav ul").style.display = 'flex';
-        }
-    })
-}
